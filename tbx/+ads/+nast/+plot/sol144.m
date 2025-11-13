d349b72caa5fcc1f7cc3accb22c47952248a371b
Fintan Healy <fintan.healy@bristol.ac.uk>
2025-11-13 23:21:42 +0000

    sol144 results extract + plotting with hdf5

--- tbx/+ads/+nast/+plot/sol144.m
+++ tbx/+ads/+nast/+plot/sol144.m
@@ -6,23 +6,18 @@ arguments
 end
     model = mni.import_matran(fullfile(bin_folder,'Source','sol144.bdf'));
     model.draw(f,plotOpts);
-    % hdf = mni.result.hdf5(fullfile(bin_folder,'bin','sol144.h5'));
-
-    f06 =  mni.result.f06(fullfile(bin_folder,'bin','sol144.f06'));
-    res_disp =  f06.read_disp;
-    res_aeroP = f06.read_aeroP;
-    res_aeroF = f06.read_aeroF;
+    
+    hdf = mni.result.hdf5(fullfile(bin_folder,'bin','sol144.h5'));
+    res_disp =  hdf.read_displacements;
+    res_aeroP = hdf.read_aero_pressure;
+    res_aeroF = hdf.read_aero_force;
 
     % apply deformation result
-    [~,i] = ismember(model.GRID.GID,res_disp.GP);
-    model.GRID.Deformation = [res_disp.dX(:,i);res_disp.dY(:,i);res_disp.dZ(:,i)];
+    [~,i] = ismember(model.GRID.GID,res_disp.GID);
+    model.GRID.Deformation = [res_disp.X(i),res_disp.Y(i),res_disp.Z(i)];
 
     %% apply aero result
     model.CAERO1.PanelPressure = res_aeroP.Cp;
-
-    f = [res_aeroF.aeroFx;res_aeroF.aeroFy;res_aeroF.aeroFz;...
-        res_aeroF.aeroMx;res_aeroF.aeroMy;res_aeroF.aeroMz];
-
-    model.CAERO1.PanelForce = f';
+    model.CAERO1.PanelForce = [res_aeroF.F,res_aeroF.M];
     model.update(plotOpts)
 end
