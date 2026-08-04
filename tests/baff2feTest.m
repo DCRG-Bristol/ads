9dff342e79c90b69881c8f6c80b575d7fa71f66f
ja14e6 <ja14996@bristol.ac.uk>
2026-08-04 13:17:37 +0100

    update tests to reflect new util directory

--- tests/baff2feTest.m
+++ tests/baff2feTest.m
@@ -16,24 +16,24 @@ classdef baff2feTest < matlab.unittest.TestCase
 
         function hingeTest(testCase,FoldAngles,FlareAngles)
             %create baff hinge
-            hv = fh.rotz(FlareAngles)*[1;0;0];
+            hv = dcrg.rotzd(FlareAngles)*[1;0;0];
             hinge = baff.Hinge("HingeVector",hv);
             hinge.Rotation = FoldAngles;
             %convert to fe
             feHinge = ads.baff.element2fe(hinge);
             %check coord systems
-            A_test = fh.rotz(FlareAngles)*fh.rotx(FoldAngles)*fh.rotz(-FlareAngles);
+            A_test = dcrg.rotzd(FlareAngles)*dcrg.rotxd(FoldAngles)*dcrg.rotzd(-FlareAngles);
             testCase.verifyEqual(feHinge.CoordSys(1).A,A_test,'AbsTol',1e-6);
-            testCase.verifyEqual(feHinge.CoordSys(2).A,fh.rotz(FlareAngles),'AbsTol',1e-6);
+            testCase.verifyEqual(feHinge.CoordSys(2).A,dcrg.rotzd(FlareAngles),'AbsTol',1e-6);
         end
         function connectionTest(testCase)
             % create a fuselage
             fus = baff.BluffBody.Cylinder(10,2,"NStations",11,"Material",baff.Material.Aluminium);
-            fus.A = fh.rotz(90);
+            fus.A = dcrg.rotzd(90);
             %create a wing
             wing = baff.Wing.UniformWing(10,0.05,0.1,baff.Material.Aluminium,1,0.4,"NStations",11);
             wing.Offset = [-5 0 1];
-            wing.A = fh.rotz(-90);
+            wing.A = dcrg.rotzd(-90);
             wing.Eta = 0.5;
             fus.add(wing);
             %create model
